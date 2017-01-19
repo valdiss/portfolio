4f876fe15be61fe8edb7de2f44f6167357163f85
valdiss <valentin.dissais@gmail.com>
2017-01-20 00:29:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$('a[href^="#"]').click(function(){
+	var the_id = $(this).attr("href");
+
+	$('html, body').animate({
+		scrollTop:$(the_id).offset().top
+	}, 'slow');
+	return false;
+});
+
+$("a").mouseup(function(){
+    $(this).blur();
+})
